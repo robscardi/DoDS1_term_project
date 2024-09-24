2415dc6776f3b8583a06e1cab3b447d0f741eddd
Roberto Scardia <roberto.scardia@gmail.com>
2024-09-24 16:07:35 +0200

    added interface for BinaryMethod

--- python_source/lib/modulus_exponential.py
+++ python_source/lib/modulus_exponential.py
@@ -3,5 +3,14 @@ from lib.utils import BitVector
 import numpy as np
 
 class ModulusExponential:
+    def __init__(self) -> None:
+        pass
     def __call__(self, n:int, base:BitVector, power:BitVector ) -> BitVector:
         return BitVector.fromint(np.mod(np.power(base.__int_value, power.__int_value), n))
+
+class BinaryMethod(ModulusExponential):
+    def __init__(self) -> None:
+        super().__init__()
+    def __call__(self, M:BitVector, e:BitVector, n:int) -> BitVector:
+        pass
+         
